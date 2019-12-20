4f0b3b58f0c91b1dfc3eee028ea56c519335418a
Sebastian Bentancurt <sebabentancurt@gmail.com>
2019-12-20 15:06:52 -0300

    Agrega instalacion de macros a la request

--- src/Laravel/LaravelCorrelateTrait.php
+++ src/Laravel/LaravelCorrelateTrait.php
@@ -6,31 +6,49 @@ use Closure;
 use Monolog\Logger;
 use Amp\Correlate\CorrelateProcessor;
 use Amp\Correlate\Correlate;
+use Illuminate\Http\Request;
 
 trait LaravelCorrelateTrait
 {
     /** @var LoggerInterface */
     protected $log;
 
-    public function correlateId()
+    /** @var Request */
+    private $request;
+
+    private $tracking_id;
+
+    /**
+     * Funciona como constructor del Trait
+     * 
+     * @param Request $request
+     * @param string $tracking_id
+     * 
+     * @return void
+     */
+    public function correlateId(Request $request, string $tracking_id = null)
     {
         $this->log = \Log::getMonolog();
+        $this->request = $request;
+        $this->tracking_id = $tracking_id;
+
+        $this->installMacrosCorrelateID();
         $this->setCorrelateID();
     }
 
     /**
-     * @param Request $request
-     * @param Closure $next
-     *
      * @return mixed
      */
-    public function setCorrelateID()
+    private function setCorrelateID()
     {
-        $correlate = (string) Correlate::id();
+        if (!$this->request->headers->has(Correlate::getHeaderName())) {
+            $tracking_id =  $this->tracking_id ? (string) Correlate::id() : $this->tracking_id;
+            $this->request->headers->set(Correlate::getHeaderName(), $tracking_id);
+        }
 
         $processor = new CorrelateProcessor(
             Correlate::getParamName(),
-            $correlate
+            $this->request->headers->get(Correlate::getHeaderName())
         );
 
         if ($this->log instanceof Logger) {
@@ -38,6 +56,36 @@ trait LaravelCorrelateTrait
         } elseif (method_exists($this->log, 'getMonolog')) {
             $this->log->getMonolog()->pushProcessor($processor);
         }
+    }
 
+    /**
+     * Install macros for request and response classes.
+     */
+    private function installMacrosCorrelateID()
+    {
+        if (!$this->request::hasMacro('hasCorrelationId')) {
+            $this->request::macro('hasCorrelationId', function() {
+                if ($this->headers->has(Correlate::getHeaderName())) {
+                    return true;
+                }
+                return false;
+            });
+        }
+
+        if (!$this->request::hasMacro('getCorrelationId')) {
+            $this->request::macro('getCorrelationId', function($default = null) {
+                if ($this->headers->has(Correlate::getHeaderName())) {
+                    return $this->headers->get(Correlate::getHeaderName());
+                }
+                return $default;
+            });
+        }
+
+        if (!$this->request::hasMacro('setCorrelationId')) {
+            $this->request::macro('setCorrelationId', function($cid)  {
+                $this->headers->set(Correlate::getHeaderName(), (string) $cid);
+                return $this;
+            });
+        }
     }
 }
